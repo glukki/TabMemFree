9f99fcaee627812b3bb8d9a78cfb27548e397567
GLuKKi <glukki.spb.ru@gmail.com>
2012-06-28 22:23:39 +0400

    Fixed tab params stored in hash

--- inject.js
+++ inject.js
@@ -7,14 +7,24 @@
  */
 
 (function(){
-    function getParameterByName(name){
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regexS = "[\\?&]" + name + "=([^&#]*)";
-        var regex = new RegExp(regexS);
-        var results = regex.exec(window.location.search);
-        if(results)
-            return decodeURIComponent(results[1].replace(/\+/g, ' '));
-        return '';
+//    Parse window.location.hash, get params encoded with encodeURIComponent, source string is:
+//    https://tabmemfree.appengine.com/blank.html#title=___&icon=___
+    function getParam(name){
+        var res = '';
+        var url = window.location.hash;
+
+        var start = url.indexOf(name);
+        var end = url.indexOf('&', start);
+        if(start > 0){
+            start += name.length + 1;
+            if(end > 0){
+                res = url.substring(start, end);
+            } else {
+                res = url.substring(start);
+            }
+        }
+
+        return decodeURIComponent(res);
     }
 
     //get message for history alteration: back
@@ -26,13 +36,18 @@
     });
 
     //set old title
-    document.title = getParameterByName('title');
+    var title = getParam('title');
+    if(title){
+        document.title = title;
+    }
 
     //set old favicon
-    var link = document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'icon';
-    link.href = getParameterByName('icon');
-    document.getElementsByTagName('head')[0].appendChild(link);
-
+    var href = getParam('icon');
+    if(href){
+        var link = document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'icon';
+        link.href = href;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
 })();
