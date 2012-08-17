37c10cdc2d3094d241eb7b2c7f26923fd78904c2
mala <github@ma.la>
2012-08-17 19:17:20 +0900

    using data uri for privacy, serverless, update/remove problem.

--- inject.js
+++ inject.js
@@ -8,49 +8,30 @@
 /*jslint browser: true, devel: true*/
 /*global chrome*/
 
-(function () {
-    "use strict";
-    var title, href, link;
-//    Parse window.location.hash, get params encoded with encodeURIComponent, source string is:
-//    https://tabmemfree.appengine.com/blank.html#title=___&icon=___
-    function getParam(name) {
-        var res = '',
-            url = window.location.hash,
-            start = url.indexOf(name),
-            end = url.indexOf('&', start);
-        if (start > 0) {
-            start += name.length + 1;
-            if (end > 0) {
-                res = url.substring(start, end);
-            } else {
-                res = url.substring(start);
-            }
-        }
+"use strict";
 
-        return decodeURIComponent(res);
-    }
+window.onfocus = function(){ history.back(); }
+window.onload = function(){
+	var param = location.href.substring(location.href.indexOf("#") + 1);
+	set_info( JSON.parse(decodeURIComponent(param)) );
+}
 
-    //get message for history alteration: back
-    chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-        if (request['do'] === 'load') {
-            sendResponse('loaded');
-            history.back();
-        }
-    });
+function set_info(msg) {
+	set_title(msg.title);
+	set_icon(msg.icon);
+}
 
-    //set old title
-    title = getParam('title');
-    if (title) {
-        document.title = title;
-    }
+//set old title
+function set_title(title) {
+	document.title = title;
+}
 
-    //set old favicon
-    href = getParam('icon');
-    if (href) {
-        link = document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'icon';
-        link.href = href;
-        document.getElementsByTagName('head')[0].appendChild(link);
-    }
-})();
+//set old favicon
+function set_icon(href) {
+	if (!href) return;
+	var link = document.createElement('link');
+	link.type = 'image/x-icon';
+	link.rel = 'icon';
+	link.href = href;
+	document.getElementsByTagName('head')[0].appendChild(link);
+}
