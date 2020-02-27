f2ecf4fc602e91f89b4171279f98ce66559deb12
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2020-02-27 19:40:52 +0300

    Use tabs.discard method; remove permissions to access personal data

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,6 @@
 /*jslint browser: true, devel: true*/
 /*global chrome, Store*/
+/* eslint no-console: ["error", { allow: ["debug"] }] */
 
 // constants
 var SETTING_ACTIVE = "active";
@@ -12,7 +13,7 @@ var DEFAULT_SETTINGS = {
   [SETTING_TICK]: 60, // seconds
   [SETTING_PINNED]: true
 };
-var PARK_URL = "https://tabmemfree.appspot.com/blank.html";
+const TABS_QUERY = { discarded: false, autoDiscardable: true };
 
 // globals
 var tabs = {}; // list of tabIDs with inactivity time
@@ -20,44 +21,52 @@ var ticker = null;
 var settings = {};
 
 // park idle tab if it is not parked yet
-function parkTab(tab) {
-  //check if parked
-  if (tab.url.substring(0, tab.url.indexOf("#")) !== PARK_URL) {
-    // forward tab to blank.html
-    var url = PARK_URL + "#title=" + encodeURIComponent(tab.title);
-    if (tab.favIconUrl) {
-      url += "&icon=" + encodeURIComponent(tab.favIconUrl);
+function parkTab(tabId) {
+  delete tabs[tabId];
+
+  chrome.tabs.discard(tabId, tab => {
+    if (chrome.runtime.lastError) {
+      return console.debug("Tab discard error:", chrome.runtime.lastError);
     }
-    chrome.tabs.update(tab.id, { url: url, selected: false });
-  }
+
+    if (!tab) {
+      return console.debug("Tab was not discarded:", tabId);
+    }
+
+    console.debug("Tab discarded:", tabId);
+  });
 }
 
 // simple timer - update inactivity time, unload timeouted tabs
 function tick() {
-  //sync
-  chrome.windows.getAll({ populate: true }, function(windows) {
+  console.debug("tick");
+  ticker = setTimeout(tick, settings.get(SETTING_TICK) * 1000);
+
+  chrome.tabs.query(TABS_QUERY, fetchedTabs => {
     // find active or pinned tabs to reset their time
-    var tabsToReset = {};
-    var skipPinned = settings.get(SETTING_PINNED);
-    windows.forEach(function(window) {
-      window.tabs.forEach(function(tab) {
-        if (tab.active || (skipPinned && tab.pinned)) {
-          tabsToReset[tab.id] = true;
-        }
-      });
+    const skipPinned = settings.get(SETTING_PINNED);
+
+    const activeTabs = new Set();
+    fetchedTabs.forEach(tab => {
+      if (tab.active || (skipPinned && tab.pinned)) {
+        activeTabs.add(tab.id);
+      }
     });
 
     // tick and find expired
-    var tickTime = settings.get(SETTING_TICK);
-    var tabTimeout = settings.get(SETTING_TIMEOUT);
-    Object.keys(tabs).forEach(function(tabId) {
-      if (tabsToReset[tabId]) {
+    const tickTime = settings.get(SETTING_TICK);
+    const tabTimeout = settings.get(SETTING_TIMEOUT);
+    Object.keys(tabs).forEach(key => {
+      const tab = tabs[key];
+
+      const tabId = tab.id;
+      if (activeTabs.has(tabId)) {
         return (tabs[tabId].time = 0);
       }
 
       tabs[tabId].time += tickTime;
       if (tabs[tabId].time >= tabTimeout) {
-        chrome.tabs.get(tabs[tabId].id, parkTab);
+        parkTab(tabId);
       }
     });
   });
@@ -67,63 +76,47 @@ function tick() {
 function init() {
   // load exclusion list
   // get all windows with tabs
-  chrome.windows.getAll({ populate: true }, function(windows) {
-    // get all tabs, init array with 0 inactive time
-    windows.forEach(function(window) {
-      window.tabs.forEach(function(tab) {
-        var id = tab.id;
-        tabs[id] = { id: id, time: 0 };
-      });
-    });
-
-    // bind events
-    ticker = setInterval(tick, settings.get(SETTING_TICK) * 1000);
-    //change icon
-    chrome.browserAction.setIcon({ path: "img/icon19.png" });
-    chrome.browserAction.setTitle({
-      title: chrome.i18n.getMessage("browserActionActive")
+  chrome.tabs.query(TABS_QUERY, fetchedTabs => {
+    fetchedTabs.forEach(tab => {
+      tabs[tab.id] = { id: tab.id, time: 0 };
     });
   });
+
+  //change icon
+  chrome.browserAction.setIcon({ path: "img/icon19.png" });
+  chrome.browserAction.setTitle({
+    title: chrome.i18n.getMessage("browserActionActive")
+  });
+
+  ticker = setTimeout(tick, settings.get(SETTING_TICK) * 1000);
 }
 
 // Events
 // tabs.onCreated - add to list
 chrome.tabs.onCreated.addListener(function(tab) {
+  console.debug("Tab created:", tab.id);
   tabs[tab.id] = { id: tab.id, time: 0 };
 });
 
 // tabs.onRemoved - load if unloaded, remove from list
 chrome.tabs.onRemoved.addListener(function(tabId) {
-  var i;
-  for (i in tabs) {
-    if (tabs.hasOwnProperty(i) && i === tabId) {
-      delete tabs[i];
-      break;
-    }
-  }
+  console.debug("Tab removed:", tabId);
+  delete tabs[tabId];
 });
 
 // tabs.onSelectionChanged - load if unloaded, reset inactivity
 chrome.tabs.onSelectionChanged.addListener(function(tabId) {
-  var i;
-  chrome.tabs.get(tabId, function(tab) {
-    if (tab.url.substring(0, tab.url.indexOf("#")) === PARK_URL) {
-      chrome.tabs.sendRequest(tabId, { do: "load" });
-    }
-  });
-  for (i in tabs) {
-    if (tabs.hasOwnProperty(i) && i === tabId) {
-      tabs[i].time = 0;
-      break;
-    }
-  }
+  console.debug("Tab activated:", tabId);
+  tabs[tabId] = { id: tabId, time: 0 };
 });
 
 // UI
 chrome.browserAction.onClicked.addListener(function() {
+  console.debug("Extension icon clicked");
+
   if (ticker) {
     //clear
-    clearInterval(ticker);
+    clearTimeout(ticker);
     tabs = {};
     ticker = null;
     chrome.browserAction.setIcon({ path: "img/icon19_off.png" });
@@ -135,6 +128,7 @@ chrome.browserAction.onClicked.addListener(function() {
     settings.set(SETTING_ACTIVE, true);
     init();
   }
+
   return false;
 });
 
