d7eef8973467f0ebcb83a907f72ba32c344f8571
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2020-03-07 18:43:03 +0300

    Move `store.js`

--- extension/rollup.config.js
+++ extension/rollup.config.js
@@ -1,11 +1,22 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-  input: "src/background.js",
-  output: {
-    file: "build/background.js",
-    format: "iife"
+export default [
+  {
+    input: "src/background.js",
+    output: {
+      file: "build/background.js",
+      format: "iife"
+    },
+    plugins: [resolve(), commonjs()]
   },
-  plugins: [resolve(), commonjs()]
-};
+  {
+    input: "src/store-old.js",
+    output: {
+      file: "build/store.js",
+      format: "iife",
+      name: "Store"
+    },
+    plugins: [resolve(), commonjs()]
+  }
+];
